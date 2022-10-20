1b4e89cb9e5d2cd65dab8b4fc19e5dd0da6d71ec
aepiercy <39595350+aepiercy@users.noreply.github.com>
2022-10-19 18:29:31 -0700

    Delete DataBoxException.java

--- DataBoxException.java
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.cs186.database.databox;
-
-@SuppressWarnings("serial")
-public class DataBoxException extends RuntimeException {
-    public DataBoxException(String message) {
-        super(message);
-    }
-}
